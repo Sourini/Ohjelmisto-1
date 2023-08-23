5c610fa611025c4a8f14325e576d1e57cba9ab02
Frans Rastas <rastas.frans@gmail.com>
2023-08-23 14:58:49 +0300

    siistin 04_karkausvuosi uusiksi (poistin turhan vuosi % 100 ehdon)

--- moduulit/mod03/04_karkausvuosi.py
+++ moduulit/mod03/04_karkausvuosi.py
@@ -1,5 +1,5 @@
 vuosi = float(input('anna vuosiluku:'))
-if vuosi % 4 == 0 and vuosi % 100 != 0 or vuosi % 100 == 0 and vuosi % 400 == 0:
+if vuosi % 4 == 0 and vuosi % 100 != 0 or vuosi % 400 == 0:
     print(f'{vuosi:.0f} on karkausvuosi!')
 else:
     print(f'{vuosi:.0f} ei ole karkausvuosi!')
