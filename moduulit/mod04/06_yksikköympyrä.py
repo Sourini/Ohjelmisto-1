7db451ee82085fd724bc384cfb6f07e7adf9e6de
Frans Rastas <rastas.frans@gmail.com>
2023-08-31 21:17:19 +0300

    mod4_06 korjattu; piin arvo lasketaan vihdoin.

--- moduulit/mod04/06_yksikköympyrä.py
+++ moduulit/mod04/06_yksikköympyrä.py
@@ -10,4 +10,5 @@ while pisteidenmaara > 0:
     y = random.uniform(-1, 1)
     if x ** 2 + y ** 2 < 1:
         pisteetympyrassa = pisteetympyrassa + 1
-print(f'{alkuperainenmaara:.0f} pisteestä {pisteetympyrassa} on ympyrän sisällä')
+piinarvo = (4 * pisteetympyrassa)/alkuperainenmaara
+print(f'{alkuperainenmaara:.0f} pisteestä {pisteetympyrassa} on ympyrän sisällä. Piin arvoksi laskettu {piinarvo}')
