6a5c9f124876c968a37b1e213559844e783e9788
Frans Rastas <rastas.frans@gmail.com>
2023-09-05 10:01:37 +0300

    mod5_03 siistitty. muutin = 0/1/2 True False flagiksi ja poistin turhan "1 tai vähemmän" checkin.

--- moduulit/mod05/03_prime.py
+++ moduulit/mod05/03_prime.py
@@ -1,15 +1,13 @@
 luku = int(input('anna kokonaisluku: '))
-prime = 0
+prime = False
 if luku >= 3:
     for i in range(2, luku):
         if luku % i != 0:
-            prime = 2
+            prime = True
         elif luku % i == 0:
-            prime = 1
+            prime = False
             break
-if prime == 0 and luku !=2:
-    print(f'luvut 1 ja sitä vähemmät eivät ole alkulukuja.')
-elif prime == 2 or luku == 2:
+if prime is True or luku == 2:
     print(f'{luku} on alkuluku.')
-elif prime == 1:
+elif prime is False:
     print(f'{luku} ei ole alkuluku.')
