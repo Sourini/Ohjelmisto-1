756e44b1b16c818dcf2872cd29c0d6270701982a
Frans Rastas <rastas.frans@gmail.com>
2023-09-05 09:48:22 +0300

    mod5_04 siistitty (turha x muuttuja poistettu)

--- moduulit/mod05/04_kaupungit.py
+++ moduulit/mod05/04_kaupungit.py
@@ -3,5 +3,5 @@ x = 0
 for i in range(5):
     kaupunki = input('anna kaupungin nimi: ')
     kaupungit.append(kaupunki)
-for i in range(5):
-    print(kaupungit[i])
+for kaupunki in range(5):
+    print(kaupungit[kaupunki])
